f49e653e56a0fed692ee63d68e485e746a9accf7
Vinicius Silva <viniciuscassianosilva13@gmail.com>
2025-07-15 16:03:57 -0300

    inicio do js

---
+++ pgcreate.js
@@ -0,0 +1,11 @@
+const nome = document.getElementById("nome").value;
+const bio = document.getElementById("bio").value;
+
+
+
+function criar(){
+if( nome ===""){
+ document.querySelector("p").innerHTML =' Coloque um nome valido';
+}
+window.alert = nome;
+}
